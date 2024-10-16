d1c312c18d4d37e37a01dfd7b3b5052ba252cd12
tirimid <tirimid@tirimid.net>
2024-10-17 01:20:57 +0400

    trying to get windows build working again

--- windows-build/setup.sh
+++ windows-build/setup.sh
@@ -4,9 +4,11 @@
 mkdir sdl2-dll
 cd sdl2-dll
 wget https://github.com/libsdl-org/SDL/releases/download/release-2.30.1/SDL2-2.30.1-win32-x64.zip
+wget https://github.com/libsdl-org/SDL_mixer/releases/download/release-2.8.0/SDL2_mixer-2.8.0-win32-x64.zip
 
 # unzip downloaded zips to get DLLs.
 unzip -o SDL2-2.30.1-win32-x64.zip
+unzip -o SDL2_mixer-2.8.0-win32-x64.zip
 cd ..
 
 # apply patches to the actual project itself.
